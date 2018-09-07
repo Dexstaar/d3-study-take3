37804d2acf4ace42d7009762d8fdaec06f9145c8
Kim, JeongHoon | RASIA <jeonghoon.kim@rakuten.com>
2018-09-07 17:58:45 +0800

    lec30

--- 4/4.1/app.js
+++ 4/4.1/app.js
@@ -6,13 +6,47 @@ for(var i=0; i<20; i++) {
     data.push(num);
 }
 
-d3.select('#chart')
-    .selectAll('div')
+// Create SVG Element
+var chart_width = 800;
+var chart_height = 400;
+var bar_padding = 5;
+var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', chart_width)
+    .attr('height', chart_height);
+
+// Bind Data and Create Bars
+svg.selectAll('rect')
     .data(data)
     .enter()
-    .append('div')
-    .attr('class', 'bar')
-    .style('height', function(d) {
-        var height = d * 3;
-        return height + 'px';
-    });
+    .append('rect')
+    .attr('x', function(d, i) {
+        return i * (chart_width / data.length);
+    })
+    .attr('y', function(d) {
+        return chart_height - d * 5;
+    })
+    .attr('width', chart_width / data.length - bar_padding)
+    .attr('height', function(d) {
+        return d * 5;
+    })
+    .attr('fill', '#7ED26D');
+
+// Create Labels
+svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(function(d) {
+        return d;
+    })
+    .attr('x', function(d, i) {
+        return i * (chart_width / data.length) +
+            (chart_width / data.length - bar_padding) / 2;
+    })
+    .attr('y', function(d) {
+        return chart_height - d * 5 + 15;
+    })
+    .attr('font-size', 14)
+    .attr('fill', '#fff')
+    .attr('text-anchor', 'middle');
